8b35cf8785c331def0da81d90183173404cbe5bd
CuiYang16 <cuiyangmail@163.com>
2019-04-10 17:57:53 +0800

    c

--- src/main/java/cn/edu/imut/jm/eureka/JmEurekaApplication.java
+++ src/main/java/cn/edu/imut/jm/eureka/JmEurekaApplication.java
@@ -2,7 +2,9 @@ package cn.edu.imut.jm.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class JmEurekaApplication {
 
