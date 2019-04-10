a37c6fbd1d5c014f21246e1a1fa1e47406cc7944
CuiYang16 <cuiyangmail@163.com>
2019-04-10 09:45:14 +0800

    first

---
+++ src/main/java/cn/edu/imut/jm/eureka/JmEurekaApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.imut.jm.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmEurekaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/edu/imut/jm/eureka/JmEurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.edu.imut.jm.eureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JmEurekaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
